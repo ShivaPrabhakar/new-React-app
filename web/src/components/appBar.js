9a029d33f3d9b2496ab8c2791e7e6a8dd8bdba55
ShivaPrabhakar <shivaprabhakarlingala@gmail.com>
2020-06-14 16:17:37 +0530

    routing changes and logout support

--- web/src/components/appBar.js
+++ web/src/components/appBar.js
@@ -4,6 +4,11 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
 import clsx from 'clsx';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
@@ -15,6 +20,18 @@ const cookies = new Cookies();
 
 
 const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  // menuButton: {
+  //   marginRight: theme.spacing(2),
+  // },
+  // title: {
+  //   display: 'none',
+  //   [theme.breakpoints.up('sm')]: {
+  //     display: 'block',
+  //   },
+  // },
   root: {
     flexGrow: 1,
     margin: theme.spacing(0)
@@ -34,10 +51,31 @@ export default function ButtonAppBar() {
   // 
  let history = useHistory();
 
+
+ const classes = useStyles();
+ const [anchorEl, setAnchorEl] = React.useState(null);
+
+ const isMenuOpen = Boolean(anchorEl);
+
+ const handleProfileMenuOpen = (event) => {
+   setAnchorEl(event.currentTarget);
+ };
+
+ 
+
+ const handleMenuClose = () => {
+  setAnchorEl(null);
+};
+
+
+
+ const menuId = 'primary-search-account-menu';
+ 
+
  const [token] = useState({
   token: cookies.get('w_authExp')
 })
-  const classes = useStyles();
+ // const classes = useStyles();
 // console.log(token);
 
 const config = {
@@ -56,6 +94,22 @@ const logoutFun = async () =>{
 
 
 }
+
+const renderMenu = (
+  <Menu
+    anchorEl={anchorEl}
+    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+    id={menuId}
+    keepMounted
+    transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+    open={isMenuOpen}
+    onClose={handleMenuClose}
+  >
+    <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+    <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    <MenuItem onClick={logoutFun}>LogOut</MenuItem>
+  </Menu>
+);
 
  const setBth = () =>{
   if(token.token === ""){
@@ -82,10 +136,23 @@ const logoutFun = async () =>{
               <Typography variant="h6" className={classes.title}>
               <Button size={"large"} disableRipple disableFocusRipple disableElevation href={'/home'} color="inherit"><strong className={classes.chatbtn} >ChatApp</strong></Button>
               </Typography>
-              <Button disableRipple disableFocusRipple disableElevation href={'/home'} color="inherit">Home</Button>
-              <Button disableRipple disableFocusRipple disableElevation onClick={logoutFun}  color="inherit">Log Out</Button>
+              
+              <div className={classes.sectionDesktop}>
+                  <IconButton
+                    edge="end"
+                    aria-label="account of current user"
+                    aria-controls={menuId}
+                    aria-haspopup="true"
+                    onClick={handleProfileMenuOpen}
+                    color="inherit"
+                  >
+                    <AccountCircle />
+                  </IconButton>
+              </div>
+          
           </Toolbar>
         </AppBar>
+        {renderMenu}
       </div>
     );
   }
